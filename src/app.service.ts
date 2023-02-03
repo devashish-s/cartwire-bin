c4272970da1842418bf7e6b97bd2dc88b4ce62a5
devashish-s <devashish.s@srmtechsol.com>
2023-02-03 18:21:53 +0530

    Secret manager

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,6 +4,37 @@ const bin_info = require('../test.json');
 
 // import * as bin_info from 'test.json';
 // gcloud secrets versions access "latest" --secret "CARTWIRE_BIN_PROD_DATABASE"
+/*
+gcloud secrets versions access latest --project=exploring-gcp-373314 --secret=CARTWIRE_BIN_PROD_DATABASE
+
+gcloud projects add-iam-policy-binding exploring-gcp-373314 --member="user:ankitdevashish@gmail.com" --role=roles/secretmanager.admin
+gcloud projects add-iam-policy-binding exploring-gcp-373314 --member="user:ankitdevashish@gmail.com" --role=roles/container.clusterAdmin
+gcloud projects add-iam-policy-binding exploring-gcp-373314 --member="user:ankitdevashish@gmail.com" --role=roles/iam.serviceAccountAdmin
+
+gcloud secrets add-iam-policy-binding CARTWIRE_BIN_PROD_DATABASE \
+    --member=serviceAccount:readonly-secrets@exploring-gcp-373314.iam.gserviceaccount.com \
+    --role='roles/secretmanager.secretAccessor'
+
+    gcloud secrets add-iam-policy-binding CARTWIRE_BIN_PROD_DATABASE \
+    --member=serviceAccount:readonly-secrets@exploring-gcp-373314.iam.gserviceaccount.com \
+    --role='roles/secretmanager.secretAccessor'
+
+    gcloud iam service-accounts add-iam-policy-binding readonly-secrets@exploring-gcp-373314.iam.gserviceaccount.com \
+    --member=serviceAccount:exploring-gcp-373314.svc.id.goog[readonly-ns/readonly-sa] \
+    --role='roles/iam.workloadIdentityUser'
+gcloud iam service-accounts add-iam-policy-binding readwrite-secrets@exploring-gcp-373314.iam.gserviceaccount.com \
+    --member=serviceAccount:exploring-gcp-373314.svc.id.goog[admin-ns/admin-sa] \
+    --role='roles/iam.workloadIdentityUser'
+
+    kubectl annotate serviceaccount readonly-sa \
+    --namespace=readonly-ns \
+    iam.gke.io/gcp-service-account=readonly-secrets@exploring-gcp-373314.iam.gserviceaccount.com
+
+
+kubectl annotate serviceaccount admin-sa \
+    --namespace=admin-ns \
+    iam.gke.io/gcp-service-account=readwrite-secrets@exploring-gcp-373314.iam.gserviceaccount.com
+*/ 
 
 @Injectable()
 export class AppService {
