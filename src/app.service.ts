440cfcf8981bf72415803749e25580af2c957017
devashish-s <devashish.s@srmtechsol.com>
2023-02-03 11:55:37 +0530

    Secret manager

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,11 +1,21 @@
 import { Injectable } from '@nestjs/common';
+const { SecretManagerServiceClient } = require('@google-cloud/secret-manager').v1;
 const bin_info = require('../test.json');
+
 // import * as bin_info from 'test.json';
 
 @Injectable()
 export class AppService {
-  getHello(): object {
+  //  getHello(): object {
+  async getHello() {
+
+    const secretmanagerClient = new SecretManagerServiceClient();
+    const request = {
+      name,
+    };
+    const response = await secretmanagerClient.getSecret(request);
+    console.log(response);
     console.log(bin_info.title);
-    return bin_info;
+    return {response, ...bin_info};
   }
 }
