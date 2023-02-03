4dfa0fbd150c1665731f0b8f5b610e1001b23759
devashish-s <devashish.s@srmtechsol.com>
2023-02-03 15:31:37 +0530

    Secret manager

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,17 +3,18 @@ const { SecretManagerServiceClient } = require('@google-cloud/secret-manager').v
 const bin_info = require('../test.json');
 
 // import * as bin_info from 'test.json';
+// gcloud secrets versions access "latest" --secret "CARTWIRE_BIN_PROD_DATABASE"
 
 @Injectable()
 export class AppService {
   //  getHello(): object {
   async getHello() {
-    const name = 'projects/exploring-gcp-373314/secrets/CARTWIRE_BIN_PROD_DATABASE'
+    const name = 'projects/exploring-gcp-373314/secrets/CARTWIRE_BIN_PROD_DATABASE/versions/latest'
     const secretmanagerClient = new SecretManagerServiceClient();
     const request = {
       name,
     };
-   // const response = await secretmanagerClient.getSecret(request);
+    const response = await secretmanagerClient.getSecret(request);
     console.log(request);
     console.log(bin_info.title);
     return {request, ...bin_info};
