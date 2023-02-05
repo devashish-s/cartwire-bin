e1cd4bc4157fea3d818ea624ed451ebbaae0c89e
devashish-s <devashish.s@srmtechsol.com>
2023-02-05 12:11:58 +0530

    mongodb agregate

---
+++ bash.sh
@@ -0,0 +1,8 @@
+SECRET_NAME="CARTWIRE_BIN_PROD_DATABASE"
+PROJECT_ID="exploring-gcp-373314"
+
+# retrieve the secret value
+SECRET_VALUE=$(gcloud secrets versions access latest --secret="$SECRET_NAME" --project="$PROJECT_ID")
+
+# store the secret value in an environment variable
+export MY_SECRET="$SECRET_VALUE"
