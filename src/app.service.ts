77fdb939596c92853fd984f89e8d926b8d54806c
devashish-s <devashish.s@srmtechsol.com>
2023-02-05 10:26:36 +0530

    mongodb agregate

--- src/app.service.ts
+++ src/app.service.ts
@@ -48,11 +48,11 @@ export class AppService {
     console.log(this.configService.get<string>('DB_CONNECT'));
     console.log(this.configService.get<string>('DB_VAR'));
 
-    let response = await accessSecretVersion("exploring-gcp-373314", "CARTWIRE_BIN_PROD_DATABASE");
+    // let response = await accessSecretVersion("exploring-gcp-373314", "CARTWIRE_BIN_PROD_DATABASE");
 
-    console.log('SECRET_PASSWORD-',this.configService.get<string>('SECRET_PASSWORD'));
+   // console.log('SECRET_PASSWORD-',this.configService.get<string>('SECRET_PASSWORD'));
 
-     console.log(response);
+     console.log("MY_SECRET");
     return { ...bin_info };
   }
 
