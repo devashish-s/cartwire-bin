18d7e7ec01fc5522edc62180d5a915e685c92563
devashish-s <devashish.s@srmtechsol.com>
2023-02-09 13:14:28 +0530

    Retailer info module

--- src/retailer_info/retailer_info.module.ts
+++ src/retailer_info/retailer_info.module.ts
@@ -4,6 +4,8 @@ import { RetailerInfoController } from './retailer_info.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { RetailerDataSchema } from './schema/retailer_info.schema';
 
+console.log("===========>>>>>>>>>>>>>>>>>>>", process.env.DB_CONNECT_CW_SCRAPING_DB);
+
 @Module({
   imports: [
     MongooseModule.forRoot(process.env.DB_CONNECT_CW_SCRAPING_DB),
