b7d6f30b1f3bb027309c550d408bce77c974f13f
devashish-s <devashish.s@srmtechsol.com>
2023-02-09 13:05:42 +0530

    Retailer info module

--- src/retailer_info/retailer_info.module.ts
+++ src/retailer_info/retailer_info.module.ts
@@ -2,12 +2,12 @@ import { Module } from '@nestjs/common';
 import { RetailerInfoService } from './retailer_info.service';
 import { RetailerInfoController } from './retailer_info.controller';
 import { MongooseModule } from '@nestjs/mongoose';
-import { RetailerData } from './schema/retailer_info.schema';
+import { RetailerDataSchema } from './schema/retailer_info.schema';
 
 @Module({
   imports: [
     MongooseModule.forRoot(process.env.DB_CONNECT_CW_SCRAPING_DB),
-    MongooseModule.forFeature([{ name: 'RetailerData', schema: RetailerData }])
+    MongooseModule.forFeature([{ name: 'RetailerInfo', schema: RetailerDataSchema }])
   ],
   controllers: [RetailerInfoController],
   providers: [RetailerInfoService]
