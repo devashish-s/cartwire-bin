30fef3c1718f58ea48cacb7363c60d6da0b2b803
devashish-s <devashish.s@srmtechsol.com>
2023-02-09 11:13:35 +0530

    Retailer info module

--- src/error_code/error_code.module.ts
+++ src/error_code/error_code.module.ts
@@ -1,13 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ErrorCodeService } from './error_code.service';
 import { ErrorCodeController } from './error_code.controller';
-import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/cw_widgets'),
-    MongooseModule.forFeature([{ name: 'BinData', schema: BinDataSchema }])
-  ],
   controllers: [ErrorCodeController],
   providers: [ErrorCodeService]
 })
