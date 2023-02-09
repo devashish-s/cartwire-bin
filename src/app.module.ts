4ab0478fbe1460ed4dba231e6a4be7bb73687a3c
devashish-s <devashish.s@srmtechsol.com>
2023-02-09 14:38:11 +0530

    Retailer info module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,11 @@ import { AppService } from './app.service';
 import { ErrorCodeModule } from './error_code/error_code.module';
 import { WidgetModule } from './widget/widget.module';
 import { ConfigModule } from '@nestjs/config';
-// import { RetailerInfoModule } from './retailer_info/retailer_info.module';
+import { RetailerInfoModule } from './retailer_info/retailer_info.module';
 
 
 @Module({
-  imports: [ConfigModule.forRoot(), ErrorCodeModule, WidgetModule],
+  imports: [ConfigModule.forRoot(), ErrorCodeModule, WidgetModule, RetailerInfoModule],
   controllers: [AppController],
   providers: [AppService],
 })
