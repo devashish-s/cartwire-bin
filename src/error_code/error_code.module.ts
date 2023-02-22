60d08b5a8215f246b156d3dafe0f28474dba962b
devashish-s <devashish.s@srmtechsol.com>
2023-02-22 10:31:10 +0530

    conflict resolved

--- src/error_code/error_code.module.ts
+++ src/error_code/error_code.module.ts
@@ -1,10 +1,9 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module, CacheInterceptor } from '@nestjs/common';
 import { ErrorCodeService } from './error_code.service';
 import { ErrorCodeController } from './error_code.controller';
 import { WidgetService } from '../widget/widget.service';
 import { WidgetModule } from '../widget/widget.module';
 import { RetailerInfoModule } from '../retailer_info/retailer_info.module';
-import { CacheModule, Module, CacheInterceptor  } from '@nestjs/common';
 
 @Module({
   imports: [
